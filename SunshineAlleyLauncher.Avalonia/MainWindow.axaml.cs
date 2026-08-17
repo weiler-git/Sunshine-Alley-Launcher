485fbb634ecc2205cad39f6eb3886910ebeb2c0c
AlfredSkallont-Halvdan <228938608+AlfredSkallont-Halvdan@users.noreply.github.com>
2026-08-17 19:13:45 +0200

    Add Avalonia project and initial Setup UI port work.

---
+++ SunshineAlleyLauncher.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SunshineAlleyLauncher.Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SunshineAlleyLauncher.Avalonia/Setup.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SunshineAlleyLauncher.Avalonia;
+
+public partial class Setup : Window
+{
+    public Setup()
+    {
+        InitializeComponent();
+    }
+}
